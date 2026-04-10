db8f322e330726db97a6ac1bac6ac5bb443905cd
AmerlyCruz <amecruz334@gmail.com>
2026-04-09 23:52:34 -0800

    Set production auth site URL

--- app/supabase-config.js
+++ app/supabase-config.js
@@ -1,5 +1,5 @@
 window.FLEXIWAY_SUPABASE_CONFIG = {
   url: "https://phvqcutmqufblpjkgkhs.supabase.co",
   anonKey: "sb_publishable_WglQgDgTN8YbYqjmvJCxSg_kWMxK38f",
-  siteUrl: ""
+  siteUrl: "https://goodfinanc.netlify.app"
 };
